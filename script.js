f0871cf77c8c1105f4189655bddb21a2245a7432
MRPCVS <charithsamarathunga1@gmail.com>
2025-01-03 09:42:57 +0530

    Cursor

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-function typewriterEffect(text, typingSpeed = 90){
-    const typewriterText = document.getElementById('typewriter');
-  typewriterText.innerHTML = "";
-  let i = 0;
-  let timer = setInterval(() => {
-      if (i < text.length){
-        typewriterText.innerHTML += text.charAt(i);
-      i++;          
-    } else {
-        clearInterval(timer)
-    }
-  }, typingSpeed); 
-}
+const words = ["Charith Samarathunga.","A Software Engineer."]
+let cursor = gsap.to('.cursor', {opacity: 0, ease: "power2.inOut", repeat: -1})
