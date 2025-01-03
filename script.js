7b9f4d1fd31d63613c2ee0275eb9a41972ef1471
MRPCVS <charithsamarathunga1@gmail.com>
2025-01-03 10:03:07 +0530

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let navbar = gsap.from(".navbar-brand", { opacity: 0, y: -50, duration: 1 })
 let link = gsap.from(".nav-link", { opacity: 0, x: -50, duration: 0.5, stagger: 0.2 })
 let section = gsap.from("section", { opacity: 0, y: 100, duration: 1, stagger: 0.3, onComplete: () => masterTl.play() })
-let image = gsap.to(".image",{duration: 7,rotation:720, repeat: 0, ease: "bounce", backgroundColor: "#ffffff", boxShadow: '0 0 20px 5px rgba(255, 255, 255, 0.8)'})
+let image = gsap.to(".image",{duration: 7,rotation:720, repeat: -1, repeatDelay: 1, ease: "bounce", backgroundColor: "#ffffff", boxShadow: '0 0 20px 5px rgba(255, 255, 255, 0.8)'})
 
 const words = ["Charith Samarathunga.","A Software Engineer."]
 
